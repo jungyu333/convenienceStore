d24c80b2bdb98436c2ace79c68b9d153a691b953
jungyu <jungyu@naver.com>
2022-11-27 16:22:45 +0900

    first commit

---
+++ client/src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import GlobalStyle from './styles/globalStyle';
+import { StyledEngineProvider } from '@mui/material';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
+root.render(
+  <>
+    <StyledEngineProvider injectFirst>
+      <GlobalStyle />
+      <App />
+    </StyledEngineProvider>
+  </>,
+);
---
+++ server/src/server.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response, NextFunction } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  res.send('welcome!');
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`
+  ################################################
+  🛡️  Server listening on port: ${process.env.PORT}🛡️
+  ################################################
+`);
+});
