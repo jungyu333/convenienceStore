88e4390e7b47fd6bef0013f878a3c8c2c65495b7
jungyu <jungyu@naver.com>
2022-11-28 15:16:48 +0900

    feat : Layout 구현 , theme color 세팅 Layout component 레이아웃 구현 , theme color 속성 세팅

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import Layout from './components/common/Layout';
 import GlobalFont from './fonts/fonts';
 import Router from './Router';
+import { theme } from './styles/theme';
 
 function App() {
   return (
-    <div>
+    <ThemeProvider theme={theme}>
       <GlobalFont />
+      <Layout />
       <Router />
-    </div>
+    </ThemeProvider>
   );
 }
 
---
+++ client/src/styles/theme.ts
@@ -0,0 +1,11 @@
+import { DefaultTheme } from 'styled-components';
+
+const colors = {
+  orange: '#f09618',
+};
+
+export type ColorsTypes = typeof colors;
+
+export const theme: DefaultTheme = {
+  colors,
+};
