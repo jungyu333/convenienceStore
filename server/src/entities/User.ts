4e95e7e0daba9e79308bb3c1e4db8572d9505915
jungyu <jungyu@naver.com>
2022-11-30 23:49:18 +0900

    feat : db 연동 세팅 , entity 생성 db 연동 세팅 완료 , BaseEntity , UserEntity 생성 완료

---
+++ server/src/entities/User.ts
@@ -0,0 +1,47 @@
+import { IsEmail, Length } from 'class-validator';
+import bcrypt from 'bcryptjs';
+import BaseEntity from './Entity';
+import { BeforeInsert, Column, Entity, Index } from 'typeorm';
+import { Expose } from 'class-transformer';
+
+@Entity('users')
+export default class User extends BaseEntity {
+  @Index()
+  @IsEmail(undefined, { message: '이메일 주소가 잘못되었습니다.' })
+  @Length(1, 255, { message: '이메일 주소는 비워둘 수 없습니다.' })
+  @Column({ unique: true })
+  email: string;
+
+  @Length(1, 32, { message: '닉네임은 1자 이상이어야 합니다' })
+  @Column()
+  nickname: string;
+
+  @Length(10, 255, { message: '비밀번호는 10자 이상이어야 합니다.' })
+  @Column({ nullable: true })
+  password: string;
+
+  @Length(1, 255)
+  @Column({ type: 'text', nullable: true })
+  avatarUrn: string | null;
+
+  @Length(1, 32)
+  @Column({ default: 0 })
+  role: number;
+
+  @Column({ default: 'local' })
+  provider: string;
+
+  @Expose()
+  get avatarUrl(): string {
+    if (this.avatarUrn) {
+      return `${process.env.SERVER_BASE_URL}/${this.avatarUrn}`;
+    } else {
+      return 'https://www.gravatar.com/avatar?d=mp&f=y';
+    }
+  }
+
+  @BeforeInsert()
+  async hashPassword() {
+    if (this.password) this.password = await bcrypt.hash(this.password, 6);
+  }
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,18 @@
 import express, { Request, Response, NextFunction } from 'express';
 import dotenv from 'dotenv';
+import { AppDataSource } from './data-source';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
+
+app.use(
+  cors({
+    origin: process.env.ORIGIN_URL,
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -18,4 +27,9 @@ app.listen(process.env.PORT, () => {
   🛡️  Server listening on port: ${process.env.PORT}🛡️
   ################################################
 `);
+  AppDataSource.initialize()
+    .then(async () => {
+      console.log('database initialized');
+    })
+    .catch(error => console.error(error));
 });
