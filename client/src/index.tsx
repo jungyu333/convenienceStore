b888f93343447b325a50322bd8e2d47a61b38dc4
jungyu <jungyu@naver.com>
2022-12-01 14:45:54 +0900

    feat : avatar 미리보기 기능 구현 유저 회원가입 avatar 업로드 미리보기 구현 완료

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import GlobalStyle from './styles/globalStyle';
 import { StyledEngineProvider } from '@mui/material';
+import { Provider } from 'react-redux';
+import store from './store/store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
   <>
-    <StyledEngineProvider injectFirst>
-      <GlobalStyle />
-      <App />
-    </StyledEngineProvider>
+    <Provider store={store}>
+      <StyledEngineProvider injectFirst>
+        <GlobalStyle />
+        <App />
+      </StyledEngineProvider>
+    </Provider>
   </>,
 );
